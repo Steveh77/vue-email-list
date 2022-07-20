80549dee1734217c37ce4f780266d4ff40c0d7c5
Steveh77 <stederosa76@gmail.com>
2022-07-20 15:31:58 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+
+console.log("vue ok",Vue)
+
+const root = new Vue ({
+    el:"#root",
+    data:{
+      
+    },
+    computed: {
+        
+    },
+    
+})
